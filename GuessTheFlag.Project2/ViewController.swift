83cc988b011443ef325c04b7ff6dadbb32e3ed79
ElijahButers <Elijahbuters@gmail.com>
2016-03-01 14:16:13 +0200

    Conntected 3 buttons as outlets.

--- GuessTheFlag.Project2/ViewController.swift
+++ GuessTheFlag.Project2/ViewController.swift
@@ -9,7 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // Outlets
 
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.

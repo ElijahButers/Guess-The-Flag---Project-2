a39ff37f9ede7bb907a2ba127b74422643938a5a
ElijahButers <Elijahbuters@gmail.com>
2016-03-01 15:14:24 +0200

    Added func askQuestion(), added to func viewDidLoad().

--- GuessTheFlag.Project2/ViewController.swift
+++ GuessTheFlag.Project2/ViewController.swift
@@ -25,6 +25,15 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         countries += ["estonia", "france", "germany", "ireland", "italy","monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
+        askQuestion()
+    }
+    
+    func askQuestion() {
+        
+        button1.setImage(UIImage(named: countries[0]), forState: .Normal)
+        button2.setImage(UIImage(named: countries[1]), forState: .Normal)
+        button3.setImage(UIImage(named: countries[2]), forState: .Normal)
     }
 
     override func didReceiveMemoryWarning() {
